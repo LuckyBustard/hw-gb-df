d77fc3c9fc3afd1188f444e9948cd019b3dc583d
Sergey Mytsykov <smytsykov@awg.ru>
2023-02-06 22:23:53 +0300

    lesson_3 Работа с бд

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
-from datetime import datetime
-import json
+from mainapp import models as mainapp_models
+from django.http import Http404
+
 
 class MainPageView(TemplateView):
     template_name = "mainapp/templates/index.html"
@@ -10,21 +11,49 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/templates/news.html"
 
     def get_context_data(self, page=1, **kwargs):
+        context = super().get_context_data(**kwargs)
         start = (page - 1) * 5
         stop = start + 5
-        context = super().get_context_data(**kwargs)
         context["page_num"] = page
-        context["news_items"] = []
-        with open('mainapp/data/news.json') as news_file:
-            news_items = json.load(news_file)
-            for news_item in news_items[start:stop]:
-                news_item["date"] = datetime.strptime(news_item['date'], "%Y-%m-%d %H-%M-%S")
-                context["news_items"].append(news_item)
+        context["news_items"] = mainapp_models.News.objects.order_by('-updated').all()[start:stop]
         return context
+
+
+class NewsDetailPageView(TemplateView):
+    template_name = "mainapp/templates/news_detail.html"
+
+    def get_context_data(self, news_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            context['news_detail'] = mainapp_models.News.objects.get(pk=news_id)
+            return context
+        except:
+            raise Http404("Новость не найдена")
 
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/templates/courses_list.html"
+
+    def get_context_data(self, page=1, **kwargs):
+        context = super().get_context_data(**kwargs)
+        start = (page - 1) * 6
+        stop = start + 6
+        context["page_num"] = page
+        context["courses"] = mainapp_models.Courses.objects.all()[start:stop]
+        return context
+
+
+class CoursesDetailPageView(TemplateView):
+    template_name = "mainapp/templates/courses_detail.html"
+
+    def get_context_data(self, course_id, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            context['course_detail'] = mainapp_models.Courses.objects.get(pk=course_id)
+            print(context['course_detail'].__dict__)
+            return context
+        except:
+            raise Http404("Курс не найден")
 
 
 class ContactsPageView(TemplateView):
