068590ebf85397e8d825161b9eedfaf62a47912f
Sergey Mytsykov <smytsykov@awg.ru>
2023-02-15 17:30:19 +0300

    lesson_6 Авторизация

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "markdownify.apps.MarkdownifyConfig",
+    "social_django",
     'mainapp',
+    'authapp',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +64,7 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             "mainapp/templates",
+            "authapp/templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -68,6 +73,9 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
@@ -105,6 +113,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "authapp.CustomUser"
+
+LOGIN_REDIRECT_URL = "mainapp:main_page"
+LOGOUT_REDIRECT_URL = "mainapp:main_page"
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
+SOCIAL_AUTH_GITHUB_KEY = "9167760eb88814bb1a60"
+SOCIAL_AUTH_GITHUB_SECRET = "b12cf89b73864301637749d48cbc651a270cf324"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -131,3 +153,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Media files
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,19 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, TemplateView, UpdateView
+from mainapp import forms as mainapp_forms
 from mainapp import models as mainapp_models
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from django.http import Http404
 
 
 class MainPageView(TemplateView):
-    template_name = "mainapp/templates/index.html"
+    template_name = "mainapp/index.html"
 
 
 class NewsPageView(TemplateView):
-    template_name = "mainapp/templates/news.html"
+    template_name = "mainapp/news_list.html"
 
     def get_context_data(self, page=1, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -20,19 +25,19 @@ class NewsPageView(TemplateView):
 
 
 class NewsDetailPageView(TemplateView):
-    template_name = "mainapp/templates/news_detail.html"
+    template_name = "mainapp/news_detail.html"
 
-    def get_context_data(self, news_id, **kwargs):
+    def get_context_data(self, pk, **kwargs):
         context = super().get_context_data(**kwargs)
         try:
-            context['news_detail'] = mainapp_models.News.objects.get(pk=news_id)
+            context['news_detail'] = mainapp_models.News.objects.get(pk=pk)
             return context
         except:
             raise Http404("Новость не найдена")
 
 
 class CoursesPageView(TemplateView):
-    template_name = "mainapp/templates/courses_list.html"
+    template_name = "mainapp/courses_list.html"
 
     def get_context_data(self, page=1, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -44,25 +49,67 @@ class CoursesPageView(TemplateView):
 
 
 class CoursesDetailPageView(TemplateView):
-    template_name = "mainapp/templates/courses_detail.html"
+    template_name = "mainapp/courses_detail.html"
 
     def get_context_data(self, course_id, **kwargs):
         context = super().get_context_data(**kwargs)
         try:
             context['course_detail'] = mainapp_models.Courses.objects.get(pk=course_id)
-            print(context['course_detail'].__dict__)
-            return context
         except:
             raise Http404("Курс не найден")
 
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_detail"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_detail"])
+        if not self.request.user.is_anonymous:
+            if not mainapp_models.CourseFeedback.objects.filter(
+                    course=context["course_detail"], user=self.request.user
+            ).count():
+                context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
+                    course=context["course_detail"], user=self.request.user
+                )
+        context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
+            course=context["course_detail"]
+        ).order_by("-created", "-rating")[:5]
+        return context
+
+
+class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
+    model = mainapp_models.CourseFeedback
+    form_class = mainapp_forms.CourseFeedbackForm
+
+    def form_valid(self, form):
+        self.object = form.save()
+        rendered_card = render_to_string("mainapp/includes/feedback_card.html", context={"item": self.object})
+        return JsonResponse({"card": rendered_card})
+
 
 class ContactsPageView(TemplateView):
-    template_name = "mainapp/templates/contacts.html"
+    template_name = "mainapp/contacts.html"
 
 
 class DocSitePageView(TemplateView):
-    template_name = "mainapp/templates/doc_site.html"
+    template_name = "mainapp/doc_site.html"
 
 
-class LoginPageView(TemplateView):
-    template_name = "mainapp/templates/login.html"
+class NewsCreateView(PermissionRequiredMixin, CreateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.add_news",)
+
+
+class NewsDetailView(DetailView):
+    model = mainapp_models.News
+
+
+class NewsUpdateView(PermissionRequiredMixin, UpdateView):
+    model = mainapp_models.News
+    fields = "__all__"
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.change_news",)
+
+
+class NewsDeleteView(PermissionRequiredMixin, DeleteView):
+    model = mainapp_models.News
+    success_url = reverse_lazy("mainapp:news")
+    permission_required = ("mainapp.delete_news",)
